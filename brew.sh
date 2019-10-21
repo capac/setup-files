824e88f943e772350bac208a36f3409935bdb1cd
capac <capac@users.noreply.github.com>
2019-10-21 19:30:52 +0100

    Script to download Homebrew packages

---
+++ brew.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# install Brew packages
+brew install git
+brew install wget
+brew install watch
+brew install pandoc
+brew install nmap
+brew install geoip
+brew install ghostscript
+brew install htop
+brew install exiftool
+brew install tree
+brew install tmux
+brew install gpg2
+brew install rlwrap
+brew install fish
+brew install mkvtoolnix
+
+# install macOS packages
+
+brew cask install firefox
+brew cask install sourcetree
+brew cask install vlc
+brew cask install skype
+brew cask install postbox
+brew cask install dash
+brew cask install iterm2
+brew cask install viscosity
+brew cask install visual-studio-code
