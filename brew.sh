e3441c73d8d83088945c54e7976cb68c176c72a1
capac <capac@users.noreply.github.com>
2019-10-22 19:43:16 +0100

    Added commands to install in Applications folders and subfolders

--- brew.sh
+++ brew.sh
@@ -18,23 +18,49 @@ brew install fish
 brew install mkvtoolnix
 brew install emacs --with-cocoa
 
-# install macOS packages
-
-brew cask install 1password
 brew cask install basictex
-brew cask install acorn
-brew cask install cocktail
-brew cask install daisydisk
-brew cask install dash
-brew cask install iterm2
-brew cask install firefox
-brew cask install omnifocus
-brew cask install postbox
-brew cask install skim
-brew cask install skype
-brew cask install slack
-brew cask install sourcetree
-brew cask install textmate
-brew cask install viscosity
-brew cask install visual-studio-code
-brew cask install vlc
+# install macOS packages in Applications/Computation folder
+brew cask install --appdir="/Applications/Computation" r
+brew cask install --appdir="/Applications/Computation" rstudio
+# install macOS packages in Applications/Intel Power Gadget folder
+brew cask install --appdir="/Applications/Intel Power Gadget" intel-power-gadget
+# install macOS packages in Applications/Internet folder
+brew cask install --appdir="/Applications/Internet" brave-browser
+brew cask install --appdir="/Applications/Internet" firefox
+brew cask install --appdir="/Applications/Internet" opera
+brew cask install --appdir="/Applications/Internet" postbox
+brew cask install --appdir="/Applications/Internet" skype
+brew cask install --appdir="/Applications/Internet" slack
+brew cask install --appdir="/Applications/Internet" spamsieve
+# install macOS packages in Applications/Maintenance folder
+brew cask install --appdir="/Applications/Maintenance" appdelete
+brew cask install --appdir="/Applications/Maintenance" carbon-copy-cloner
+brew cask install --appdir="/Applications/Maintenance" coconutbattery
+brew cask install --appdir="/Applications/Maintenance" cocktail
+brew cask install --appdir="/Applications/Maintenance" daisydisk
+brew cask install --appdir="/Applications/Maintenance" drivedx
+brew cask install --appdir="/Applications/Maintenance" istat-menus
+brew cask install --appdir="/Applications/Maintenance" launchpad-manager
+brew cask install --appdir="/Applications/Maintenance" onyx
+# install macOS packages in Applications/Multimedia 
+brew cask install --appdir="/Applications/Multimedia" acorn
+# install macOS packages in Applications/Office folder
+brew cask install --appdir="/Applications/Office" omnifocus
+brew cask install --appdir="/Applications/Office" skim
+# install macOS packages in Applications/Programming folder
+brew cask install --appdir="/Applications/Programming" iterm2
+brew cask install --appdir="/Applications/Programming" dash
+brew cask install --appdir="/Applications/Programming" textmate
+# install macOS packages in Applications/Security folder
+brew cask install --appdir="/Applications/Security" micro-snitch
+brew cask install --appdir="/Applications/Security" 1password
+# install macOS packages in Applications/Sports folder
+brew cask install --appdir="/Applications/Sports" rubitrack-pro
+
+
+brew cask install --appdir="/Applications" sourcetree
+
+brew cask install --appdir="/Applications/Security" viscosity
+brew cask install --appdir="/Applications" visual-studio-code
+brew cask install --appdir="/Applications" vlc
+brew cask install --appdir="/Applications/Security" wifi-explorer
