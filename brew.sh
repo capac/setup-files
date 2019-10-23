828a2c1a0792ae5d89b216605e832b07f03f58a5
capac <capac@users.noreply.github.com>
2019-10-23 17:04:50 +0100

    Create Application subfolders if non-existing

--- brew.sh
+++ brew.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p Applications/{Computation, Intel Power Gadget, Internet, Maintenance, Multimedia/{Graphics}, Office, Programming, Security, Sports}
+
 # install Brew packages
 brew cask install basictex
 brew install emacs --with-cocoa
@@ -32,11 +34,14 @@ brew cask install --appdir="/Applications" ntfs-mac
 brew cask install --appdir="/Applications" postgres
 brew cask install --appdir="/Applications" psequel
 brew cask install --appdir="/Applications" the-unarchiver
+
 # install macOS packages in Applications/Computation folder
 brew cask install --appdir="/Applications/Computation" r
 brew cask install --appdir="/Applications/Computation" rstudio
+
 # install macOS packages in Applications/Intel Power Gadget folder
 brew cask install --appdir="/Applications/Intel Power Gadget" intel-power-gadget
+
 # install macOS packages in Applications/Internet folder
 brew cask install --appdir="/Applications/Internet" brave-browser
 brew cask install --appdir="/Applications/Internet" firefox
@@ -47,6 +52,7 @@ brew cask install --appdir="/Applications/Internet" postbox
 brew cask install --appdir="/Applications/Internet" skype
 brew cask install --appdir="/Applications/Internet" slack
 brew cask install --appdir="/Applications/Internet" spamsieve
+
 # install macOS packages in Applications/Maintenance folder
 brew cask install --appdir="/Applications/Maintenance" appdelete
 brew cask install --appdir="/Applications/Maintenance" carbon-copy-cloner
@@ -57,12 +63,14 @@ brew cask install --appdir="/Applications/Maintenance" drivedx
 brew cask install --appdir="/Applications/Maintenance" istat-menus
 brew cask install --appdir="/Applications/Maintenance" launchpad-manager
 brew cask install --appdir="/Applications/Maintenance" onyx
+
 # install macOS packages in Applications/Multimedia 
 brew cask install --appdir="/Applications/Multimedia/Graphics" acorn
 brew cask install --appdir="/Applications/Multimedia/Graphics" xee
 brew cask install --appdir="/Applications/Multimedia" handbrake
 brew cask install --appdir="/Applications/Multimedia" vlc
 brew cask install --appdir="/Applications/Multimedia" xld
+
 # install macOS packages in Applications/Office folder
 brew cask install --appdir="/Applications/Office" anki
 brew cask install --appdir="/Applications/Office" calibre
@@ -70,6 +78,7 @@ brew cask install --appdir="/Applications/Office" devonagent
 brew cask install --appdir="/Applications/Office" devonthink
 brew cask install --appdir="/Applications/Office" omnifocus
 brew cask install --appdir="/Applications/Office" skim
+
 # install macOS packages in Applications/Programming folder
 brew cask install --appdir="/Applications/Programming" brackets
 brew cask install --appdir="/Applications/Programming" dash
@@ -79,13 +88,16 @@ brew cask install --appdir="/Applications/Programming" pycharm
 brew cask install --appdir="/Applications/Programming" sourcetree
 brew cask install --appdir="/Applications/Programming" textmate
 brew cask install --appdir="/Applications/Programming" visual-studio-code
+
 # install macOS packages in Applications/Security folder
 brew cask install --appdir="/Applications/Security" 1password
 brew cask install --appdir="/Applications/Security" micro-snitch
 brew cask install --appdir="/Applications/Security" tor-browser
 brew cask install --appdir="/Applications/Security" viscosity
 brew cask install --appdir="/Applications/Security" wifi-explorer
+
 # install macOS packages in Applications/Sports folder
 brew cask install --appdir="/Applications/Sports" rubitrack-pro
+
 # install macOS packages in Applications/Utilities folder
 brew cask install --appdir="/Applications/Utilities" xquartz
