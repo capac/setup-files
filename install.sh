adb437c8006e9fc55511f5b6e91b13c1c30715e5
capac <capac@users.noreply.github.com>
2019-12-03 11:57:35 +0000

    Updated Miniconda installation process and added echo statements

--- install.sh
+++ install.sh
@@ -2,30 +2,38 @@
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: install.sh <home_directory>"
+    echo "'sudo' is required for write premissions"
     exit 1
 fi
 
 homedir="/Users/angelo"
 
-# install Xcode CLI tools
+echo "Installing the Xcode CLI tools..."
 sudo xcode-select --install
+echo
 
-# install Homebrew
+echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo
 
-# create Application subfolders
+echo "Creating the Applications subfolders..."
 mkdir -p Applications/{Computation, Intel Power Gadget, Internet, Maintenance, Multimedia/{Graphics}, Office, Programming, Security, Sports}
+echo
 
-# download the latest Miniconda distribution
+echo "Downloading the latest Miniconda distribution for macOS..."
 curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh" > ${homedir}
+echo
+echo "Installing the latest Miniconda distribution for macOS..."
+bash Miniconda3-latest-MacOSX-x86_64.sh -b -f -t -p /Applications/Computation/miniconda3
+echo
 
-# run the Homebrew Script
+echo "Running the Homebrew Script..."
 ./brew.sh
 
-# download .gitconfig file
+echo "Downloading the .gitconfig file..."
 curl "https://raw.githubusercontent.com/capac/setup-files/master/.gitconfig" > ${homedir}/.gitconfig
 
-# download fish shell config files
+echo "Downloading the fish shell config files..."
 mkdir -p ${homedir}/.conda/fish/functions
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/fish/config.fish" > ${homedir}/.conda/fish/config.fish
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/fish/fish_variables" > ${homedir}/.conda/fish/fish_variables
@@ -35,17 +43,22 @@ curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/fish/f
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/fish/functions/la.fish" > ${homedir}/.conda/fish/functions/la.fish
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/fish/functions/ls.fish" > ${homedir}/.conda/fish/functions/ls.fish
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/fish/functions/pdf.fish" > ${homedir}/.conda/fish/functions/pdf.fish
+echo
 
-# download IPython config files
+echo "Downloading the IPython config files..."
 mkdir -p ${homedir}/.ipython/profile_default/
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/ipython/ipython_config.py" > ${homedir}/.ipython/profile_default/ipython_config.py
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/ipython/ipython_kernel_config.py" > ${homedir}/.ipython/profile_default/ipython_kernel_config.py
+echo
 
-# dowmload iTerm2 preference and color-template files
+echo "Dowmloading the iTerm2 preference and color-template files..."
 mkdir -p ${homedir}/Library/Preferences
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/iterm2/com.googlecode.iterm2.plist" > ${homedir}/Library/Preferences/com.googlecode.iterm2.plist
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/iterm2/MyMonokaiPro-FilterMachine.itermcolors" > ${homedir}/MyMonokaiPro-FilterMachine.itermcolors
+echo
 
-# download VSCode config files
+echo "Downloading the VSCode config files..."
 mkdir -p ${homedir}/Library/Application\ Support/Code/User/
 curl "https://raw.githubusercontent.com/capac/setup-files/master/settings/visual-studio-code/settings.json" > ${homedir}/Library/Application\ Support/Code/User/settings.json
+echo
+echo "Done!"
