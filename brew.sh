85278d833cef47b027aac60530010752b9a36a7a
capac <capac@users.noreply.github.com>
2025-07-16 08:42:17 +0100

    Refactored code using for loops

--- brew.sh
+++ brew.sh
@@ -1,129 +1,28 @@
 #!/usr/bin/env bash
 
-echo "Installing brew packages..."
-brew cask install basictex
-brew cask install miniconda
-brew install coreutils
-brew install exiftool
-brew install ffmpeg
-brew install fish
-brew install geoip
-brew install git
-brew install golang
-brew install gnupg
-brew install heroku/brew/heroku
-brew install htop
-brew install imagemagick
-brew install lame
-brew install mkvtoolnix
-brew install nmap
-brew install pandoc
-brew install readline
-brew install ruby
-brew install tmux
-brew install tree
-brew install ufraw
-brew install watch
-brew install wget
-brew install youtube-dl
+echo "Installing Homebrew packages..."
+brew install --cask basictex font-hack-nerd-font
+brew install awscli exiftool eza ffmpeg fish gh git-lfs \
+             go htop hugo julia nmap pandoc php pyenv rclone \
+             tcl-tk@8 tmux wget yt-dlp zlib 
 echo
 
-echo "Installing brew cask packages in the Applications folder..."
-brew cask install --appdir="/Applications" alfred
-brew cask install --appdir="/Applications" backblaze
-brew cask install --appdir="/Applications" fantastical
-brew cask install --appdir="/Applications" little-snitch
-brew cask install --appdir="/Applications" logitech-options
-brew cask install --appdir="/Applications" macupdater
-brew cask install --appdir="/Applications" nextcloud
-brew cask install --appdir="/Applications" ntfs-mac
-brew cask install --appdir="/Applications" postgres
-brew cask install --appdir="/Applications" psequel
-brew cask install --appdir="/Applications" timemachineeditor
-brew cask install --appdir="/Applications" the-unarchiver
-echo
+declare -A CASK_GROUPS=(
+  ["/Applications"]="alfred backblaze fantastical little-snitch logitech-options macupdater ntfs-mac postgres"
+  ["/Applications/Computation"]="r rstudio"
+  ["/Applications/Internet"]="firefox forklift qbittorrent slack thunderbird"
+  ["/Applications/Maintenance"]="carbon-copy-cloner coconutbattery cocktail daisydisk drivedx geekbench istat-menus launchpad-manager onyx"
+  ["/Applications/Multimedia/Graphics"]="acorn imageoptim"
+  ["/Applications/Multimedia"]="handbrake movist-pro pocket-casts vlc xld"
+  ["/Applications/Office"]="calibre devonagent devonthink omnifocus skim vmware-fusion13"
+  ["/Applications/Programming"]="dash docker iterm2 sourcetree sublimetext textmate visual-studio-code"
+  ["/Applications/Security"]="1password micro-snitch"
+)
 
-echo "Installing brew cask packages in the Applications/Computation folder..."
-brew cask install --appdir="/Applications/Computation" gfortran
-brew cask install --appdir="/Applications/Computation" julia
-brew cask install --appdir="/Applications/Computation" r
-brew cask install --appdir="/Applications/Computation" rstudio
-echo
+for dir in "${!CASK_GROUPS[@]}"; do
+  echo "Installing cask apps in $dir..."
+  brew install --cask --appdir="$dir" ${CASK_GROUPS[$dir]}
+  echo
+done
 
-echo "Installing brew cask packages in the Applications/Intel Power Gadget folder..."
-brew cask install --appdir="/Applications/Intel Power Gadget" intel-power-gadget
-echo
-
-echo "Installing brew cask packages in the Applications/Internet folder..."
-brew cask install --appdir="/Applications/Internet" firefox
-brew cask install --appdir="/Applications/Internet" forklift
-brew cask install --appdir="/Applications/Internet" qbittorrent
-brew cask install --appdir="/Applications/Internet" postbox
-brew cask install --appdir="/Applications/Internet" skype
-brew cask install --appdir="/Applications/Internet" slack
-brew cask install --appdir="/Applications/Internet" spamsieve
-brew cask install --appdir="/Applications/Internet" thunderbird
-echo
-
-echo "Installing brew cask packages in the Applications/Maintenance folder..."
-brew cask install --appdir="/Applications/Maintenance" appdelete
-brew cask install --appdir="/Applications/Maintenance" carbon-copy-cloner
-brew cask install --appdir="/Applications/Maintenance" coconutbattery
-brew cask install --appdir="/Applications/Maintenance" cocktail
-brew cask install --appdir="/Applications/Maintenance" daisydisk
-brew cask install --appdir="/Applications/Maintenance" drivedx
-brew cask install --appdir="/Applications/Maintenance" geekbench
-brew cask install --appdir="/Applications/Maintenance" istat-menus
-brew cask install --appdir="/Applications/Maintenance" launchpad-manager
-brew cask install --appdir="/Applications/Maintenance" onyx
-echo
-
-echo "Installing brew cask packages in the Applications/Multimedia..."
-brew cask install --appdir="/Applications/Multimedia/Graphics" acorn
-brew cask install --appdir="/Applications/Multimedia/Graphics" 
-brew cask install --appdir="/Applications/Multimedia/Graphics" imageoptim
-brew cask install --appdir="/Applications/Multimedia/Graphics" xee
-brew cask install --appdir="/Applications/Multimedia" handbrake
-brew cask install --appdir="/Applications/Multimedia" movist-pro
-brew cask install --appdir="/Applications/Multimedia" pocket-casts
-brew cask install --appdir="/Applications/Multimedia" vlc
-brew cask install --appdir="/Applications/Multimedia" xld
-echo
-
-echo "Installing brew cask packages in the Applications/Office folder..."
-brew cask install --appdir="/Applications/Office" anki
-brew cask install --appdir="/Applications/Office" calibre
-brew cask install --appdir="/Applications/Office" devonagent
-brew cask install --appdir="/Applications/Office" devonthink
-brew cask install --appdir="/Applications/Office" omnifocus
-brew cask install --appdir="/Applications/Office" skim
-brew cask install --appdir="/Applications/Office" vmware-fusion11
-echo
-
-echo "Installing brew cask packages in the Applications/Programming folder..."
-brew cask install --appdir="/Applications/Programming" atom
-brew cask install --appdir="/Applications/Programming" dash
-brew cask install --appdir="/Applications/Programming" docker
-brew cask install --appdir="/Applications/Programming" iterm2
-brew cask install --appdir="/Applications/Programming" macvim
-brew cask install --appdir="/Applications/Programming" pluralsight
-brew cask install --appdir="/Applications/Programming" postman
-brew cask install --appdir="/Applications/Programming" pycharm-ce-with-anaconda-plugin
-brew cask install --appdir="/Applications/Programming" sourcetree
-brew cask install --appdir="/Applications/Programming" textmate
-brew cask install --appdir="/Applications/Programming" visual-studio-code
-echo
-
-echo "Installing brew cask packages in the Applications/Security folder..."
-brew cask install --appdir="/Applications/Security" 1password
-brew cask install --appdir="/Applications/Security" micro-snitch
-brew cask install --appdir="/Applications/Security" tor-browser
-brew cask install --appdir="/Applications/Security" viscosity
-echo
-
-echo "Installing brew cask packages in the Applications/Sports folder..."
-brew cask install --appdir="/Applications/Sports" suunto-moveslink2
-brew cask install --appdir="/Applications/Sports" rubitrack-pro
-echo
 echo "Done with brew.sh!"
-echo
